fbf90eb3e9a20494cf4e5768c84046e6041c7143
Brenda Vergara Martínez <81877344+BrendaVMtz@users.noreply.github.com>
2023-10-24 14:58:03 -0600

    init project ASDI

---
+++ 2_Parcial/ASDI/src/main/java/com/mycompany/asdi/ASDI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asdi;
+
+/**
+ *
+ * @author leo
+ */
+public class ASDI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
